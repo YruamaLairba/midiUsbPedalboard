988db47a7b869ed3636b76911e5ca4da0613fd59
YruamaLairba <Yruama_Lairba@hotmail.com>
2017-05-22 19:34:44 +0200

    Implement many fonction of Preset for sw conf

--- preset.cpp
+++ preset.cpp
@@ -1,4 +1,108 @@
 #include "preset.h"
 
+//ctor
+Preset::Preset(){}
 
+//void Preset::init()
+//{
+//}
 
+FsMode Preset::get_fsMode(uint8_t fsNumber)
+{
+  if (fsNumber < nbFsConfigs)
+  {
+    return fsConfigs[fsNumber].fsMode;
+  }
+  else
+  {
+    return 0xFF; //error code
+  }
+}
+
+void Preset::set_fsMode(uint8_t fsNumber, FsMode fsMode)
+{
+  if (fsNumber < nbFsConfigs)
+  {
+    if (fsConfigs[fsNumber].fsMode != fsMode)
+    {
+      fsConfigs[fsNumber].fsMode = fsMode;
+      isModified = true;
+    }
+  }
+  return;
+}
+
+FsCommand Preset::get_fsCommand(uint8_t fsNumber)
+{
+  if (fsNumber < nbFsConfigs)
+  {
+    return fsConfigs[fsNumber].fsCommand;
+  }
+  else
+  {
+    return 0xFF; //error code
+  }
+}
+
+void Preset::set_fsCommand(uint8_t fsNumber, FsCommand fsCommand)
+{
+  if (fsNumber < nbFsConfigs)
+  {
+    if(fsConfigs[fsNumber].fsCommand != fsCommand)
+    {
+      fsConfigs[fsNumber].fsCommand = fsCommand;
+      isModified = true;
+    }
+  }
+  return;
+}
+
+ExpMode Preset::get_expMode(uint8_t expNumber)
+{
+  if (expNumber < nbExpConfigs)
+  {
+    return expConfigs[expNumber].expMode;
+  }
+  else
+  {
+    return 0xFF; //error code
+  }
+}
+
+void Preset::set_expMode(uint8_t expNumber, ExpMode expMode)
+{
+  if (expNumber < nbExpConfigs)
+  {
+    if (expConfigs[expNumber].expMode != expMode)
+    {
+      expConfigs[expNumber].expMode = expMode;
+      isModified = true;
+    }
+  }
+  return;
+}
+
+ExpCommand Preset::get_expCommand(uint8_t expNumber)
+{
+  if (expNumber < nbExpConfigs)
+  {
+    return expConfigs[expNumber].expCommand;
+  }
+  else
+  {
+    return 0xFF; //error code
+  }
+}
+
+void Preset::set_expCommand(uint8_t expNumber, ExpCommand expCommand)
+{
+  if (expNumber < nbExpConfigs)
+  {
+    if (expConfigs[expNumber].expCommand != expCommand)
+    {
+      expConfigs[expNumber].expCommand = expCommand;
+      isModified = true;
+    }
+  }
+  return;
+}
--- preset.h
+++ preset.h
@@ -4,25 +4,50 @@
 #include <stdint.h>
 #include "footswitch.h"
 
+typedef uint8_t FsMode;
+typedef uint8_t FsCommand;
+struct FsConfig
+{
+  FsMode fsMode;
+  FsCommand fsCommand;
+};
 
+typedef uint8_t ExpMode;
+typedef uint8_t ExpCommand;
+struct ExpConfig
+{
+  ExpMode expMode;
+  ExpCommand expCommand;
+};
 
+#warning "replace litteral values by an abstract value"
 class Preset
 {
   private:
-    footswitch* footswitches;
-    uint8_t nbFootswitches;
-    
+    FsConfig fsConfigs [4];
+    static const uint8_t nbFsConfigs = 4 ;
+    ExpConfig expConfigs [1];
+    static const uint8_t nbExpConfigs = 1;
+    uint8_t number; //number of the currently loaded preset
+    static const uint8_t nbPreset = 128;
+    bool isModified; //is the current preset modified ?
   public:
-    Preset(footswitch* footswitches, uint8_t nbFootswitches):
-    footswitches(footswitches),
-    nbFootswitches(nbFootswitches)
-    {}
+    Preset();
 
-    //save(int8_t presetNumber);
-    //load(int8_t presetNumber);
+    //void init(); //for startup initialization
 
-    footswitch* get_fs(uint8_t);
-    
+    FsMode get_fsMode(uint8_t fsNumber);
+    void set_fsMode(uint8_t fsNumber, FsMode fsMode);
+    FsCommand get_fsCommand(uint8_t fsNumber);
+    void set_fsCommand(uint8_t fsNumber, FsCommand fsCommand);
+    ExpMode get_expMode(uint8_t expNumber);
+    void set_expMode(uint8_t expNumber, ExpMode expMode);
+    ExpCommand get_expCommand(uint8_t expNumber);
+    void set_expCommand(uint8_t expNumber, ExpCommand expCommand);
+    //void save(int8_t presetNumber);
+    //void load(int8_t presetNumber);
+    //uint8_t get_presetNumber();
+    //bool isMofified();
 };
 
 
