690dea5dd4dbeb157e71e71039bde6ef3801175d
YruamaLairba <Yruama_Lairba@hotmail.com>
2018-07-26 22:08:49 +0200

    check and force validity when change/load midi channel

--- midiPedalboard/global_setting.cpp
+++ midiPedalboard/global_setting.cpp
@@ -3,11 +3,15 @@
 void GlobalSetting::init()
 {
   EEPROM.get(eeprom_midi_channel_start,midi_channel_);
+  if (midi_channel_ > 15) midi_channel_ = 0;
 }
+
+
 
 void GlobalSetting::set_midi_channel(uint8_t midi_channel)
 {
   midi_channel_=midi_channel;
+  if (midi_channel_ > 15) midi_channel_ = 0;
   EEPROM.put(eeprom_midi_channel_start,midi_channel);
 }
 
