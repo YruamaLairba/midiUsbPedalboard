4941c68e74a9098085ee66b3213fd0d50c73df28
YruamaLairba <Yruama_Lairba@hotmail.com>
2018-08-10 22:50:34 +0200

    create macro to enable warning in specific section

---
+++ midiPedalboard/warnings.h
@@ -0,0 +1,10 @@
+#ifndef WARNINGS_H
+#define WARNINGS_H
+
+#define WARNINGS \
+  _Pragma ("GCC diagnostic warning \"-Wall\"")\
+  _Pragma ("GCC diagnostic warning \"-Wextra\"")\
+  _Pragma ("GCC diagnostic warning \"-Wsign-conversion\"")\
+  _Pragma ("GCC diagnostic warning \"-pedantic\"") 
+
+#endif //WARNINGS_H
