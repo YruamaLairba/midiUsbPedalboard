35ef2f27beabaf2700f8e80120662e6ed778ab96
YruamaLairba <Yruama_Lairba@hotmail.com>
2018-08-11 16:47:53 +0200

    create macros to disable many warning for specific section

--- midiPedalboard/warnings.h
+++ midiPedalboard/warnings.h
@@ -1,6 +1,17 @@
 #ifndef WARNINGS_H
 #define WARNINGS_H
 
+#define DIAGNOSTIC_IGNORE_ALL \
+  _Pragma ("GCC diagnostic ignored \"-Wall\"")\
+  _Pragma ("GCC diagnostic ignored \"-Wextra\"")\
+  _Pragma ("GCC diagnostic ignored \"-Wsign-conversion\"")\
+  _Pragma ("GCC diagnostic ignored \"-Wignored-qualifiers\"")\
+  _Pragma ("GCC diagnostic ignored \"-Wunused-variable\"")\
+  _Pragma ("GCC diagnostic ignored \"-pedantic\"") 
+
+#define DIAGNOSTIC_POP _Pragma ("GCC diagnostic pop")
+#define DIAGNOSTIC_PUSH _Pragma ("GCC diagnostic push")
+
 #define WARNINGS \
   _Pragma ("GCC diagnostic warning \"-Wall\"")\
   _Pragma ("GCC diagnostic warning \"-Wextra\"")\
