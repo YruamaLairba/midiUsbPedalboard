ffcd683d2789b0cee5da8fd638961d3dcf0b414c
YruamaLairba <Yruama_Lairba@hotmail.com>
2018-08-21 15:46:21 +0200

    create accessor for fs and exp pedal

--- midiPedalboard/controller_system.cpp
+++ midiPedalboard/controller_system.cpp
@@ -30,4 +30,43 @@ void ControllerSystem::process()
   }
 }
 
+void ControllerSystem::get_fs_command(uint8_t fs_num)
+{
+  return fs_tab_[fs_num].get_command();
+}
+
+void ControllerSystem::set_fs_command(uint8_t fs_num, uint8_t fs_cmd)
+{
+  fs_tab_[fs_num].set_command(fs_cmd);
+}
+
+void ControllerSystem::get_fs_mode(uint8_t fs_num)
+{
+  return fs_tab_[fs_num].get_mode();
+}
+
+void ControllerSystem::set_fs_mode(uint8_t fs_num, uint8_t fs_mode)
+{
+  fs_tab_[fs_num].set_mode(fs_mode);
+}
+
+void ControllerSystem::get_exp_command(uint8_t exp_num)
+{
+  return exp_tab_[exp_num].get_command();
+}
+
+void ControllerSystem::set_exp_command(uint8_t exp_num, uint8_t exp_cmd)
+{
+  exp_tab_[exp_num].set_command(exp_cmd);
+}
+
+void ControllerSystem::get_exp_mode(uint8_t exp_num)
+{
+  return exp_tab_[exp_num].get_mode();
+}
+
+void ControllerSystem::set_exp_mode(uint8_t exp_num, uint8_t exp_mode)
+{
+  exp_tab_[exp_num].set_mode(exp_mode);
+}
 
--- midiPedalboard/controller_system.h
+++ midiPedalboard/controller_system.h
@@ -22,6 +22,16 @@ class ControllerSystem
     ControllerSystem();
     void init();
     void process();
+    //accessor for footswitch
+    void get_fs_command(uint8_t fs_num);
+    void set_fs_command(uint8_t fs_num, uint8_t fs_cmd);
+    void get_fs_mode(uint8_t fs_num);
+    void set_fs_mode(uint8_t fs_num, uint8_t fs_mode);
+    //accessor for exp pedal
+    void get_exp_command(uint8_t exp_num);
+    void set_exp_command(uint8_t exp_num, uint8_t exp_cmd);
+    void get_exp_mode(uint8_t exp_num);
+    void set_exp_mode(uint8_t exp_num, uint8_t exp_mode);
 };
 
 #endif //CONTROLLER_SYSTEM_H
