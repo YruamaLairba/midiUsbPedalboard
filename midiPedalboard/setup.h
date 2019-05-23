f8779ceb947c403a84d181e2424014d90a2f3b85
YruamaLairba <Yruama_Lairba@hotmail.com>
2019-05-23 20:35:46 +0200

    change pinout

--- midiPedalboard/setup.h
+++ midiPedalboard/setup.h
@@ -9,7 +9,7 @@ DIAGNOSTIC_POP
 //footswitch
 const uint8_t nbFs = 4;
 const uint8_t fsPins[] = {9, 10, 11, 12}; //footswitch input pin
-const uint8_t fsLedPins[] = {A0, A1, A2, A3};//footswitch led output pin
+const uint8_t fsLedPins[] = {A3, A2, A1, A0};//footswitch led output pin
 
 //expression pedal
 const uint8_t nbExp = 1;
@@ -17,15 +17,15 @@ const uint8_t expPins[] = {A5};
 
 //OLED display
 const uint8_t displayCs = 4;
-const uint8_t displayDc = 5;
-const uint8_t displayReset = 6;
+const uint8_t displayDc = 3;
+const uint8_t displayReset = 2;
 
 //rotary coder
-const uint8_t rotaryPinA = 2;
-const uint8_t rotaryPinB = 3;
+const uint8_t rotaryPinA = 8;
+const uint8_t rotaryPinB = 7;
 
 //ok and cancel buttons
-const uint8_t buttonOkPin = 7;
-const uint8_t buttonCancelPin = 8;
+const uint8_t buttonOkPin = 6;
+const uint8_t buttonCancelPin = 5;
 
 #endif //SETUP_H
